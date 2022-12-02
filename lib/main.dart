fcd66a28326c56c0590abac1543c34ee68f217c5
Aaron Clarke <aaclarke@google.com>
2022-12-01 17:08:39 -0800

    Started caching the documents directory on the agent.

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,27 @@ import 'package:isolate_agents/isolate_agents.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:flutter/services.dart' show rootBundle;
 
-/// State held by the [Agent].  It just stores the last decoded message and the
-/// number decoded so far.  There is no need to cache previously decoded
-/// messages since they are stored in the widgets.
+/// State held by the [Agent].  It just stores the last decoded message and
+/// documents directory so it doesn't have to be queried each time.
 class _DecoderState {
-  _DecoderState(this.lastDecodedMessage);
+  _DecoderState(this.documentsDir, this.lastDecodedMessage);
+  final Directory? documentsDir;
+
+  /// A null value means we've reached the end of the messages.
   final String? lastDecodedMessage;
 }
 
-// This is the agent that is used to seed the fake messages and decrypt them.
+/// This is the agent that will be used to decrypt messages as we receive the
+/// request to do so.
 Agent<_DecoderState>? _agent;
 
+/// Getter for the singleton [Agent] for decoding.
 Future<Agent<_DecoderState>> _getAgent() async {
-  _agent ??= await Agent.create(_DecoderState(null));
+  _agent ??= await Agent.create(_DecoderState(null, null));
   return _agent!;
 }
 
+/// A simple encoding method, rot13.
 String _rot13Encode(String input) {
   List<int> output = [];
 
@@ -41,6 +46,7 @@ String _rot13Encode(String input) {
   return const AsciiDecoder().convert(output);
 }
 
+/// The decoder for [_rot13Encode].
 String _rot13Decode(String input) {
   List<int> output = [];
 
@@ -61,15 +67,13 @@ String _rot13Decode(String input) {
 }
 
 Future<void> _loadMessage(Agent<_DecoderState> agent, int index) async {
-  Directory documentsPath =
-      await path_provider.getApplicationDocumentsDirectory();
   agent.send((state) {
-    File file = File('${documentsPath.path}/$index.rot');
+    File file = File('${state.documentsDir!.path}/$index.rot');
     if (file.existsSync()) {
       String encoded = file.readAsStringSync();
-      return _DecoderState(_rot13Decode(encoded));
+      return _DecoderState(state.documentsDir, _rot13Decode(encoded));
     } else {
-      return _DecoderState(null);
+      return _DecoderState(state.documentsDir, null);
     }
   });
 }
@@ -171,19 +175,35 @@ class MyHomePage extends StatefulWidget {
   }
 }
 
+Future<void> _setDocumentsDir(
+    Agent<_DecoderState> agent, Directory documentsDir) {
+  return agent
+      .send((state) => _DecoderState(documentsDir, state.lastDecodedMessage));
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _count = 0;
-  // We cache the decoded messages on the ui thread since the scroll view's
+  // We cache the decoded messages on the root isolate since the scroll view's
   // offset will jump to the top if it doesn't synchronously know the size of a
   // widget.
   final List<String> _decodedMessages = [];
 
-  Future<void> startCounting() async {
+  /// This sets the [Agent]'s documents directory then will asynchronously fire
+  /// new messages loading jobs every second.
+  Future<void> startLoadingMessages() async {
+    final Agent<_DecoderState> agent = await _getAgent();
+
+    // Store the documents directory on the agent so it doesn't have to be
+    // queried every time.
+    Directory documentsDir =
+        await path_provider.getApplicationDocumentsDirectory();
+    await _setDocumentsDir(agent, documentsDir);
+
     bool keepLoading = true;
     while (keepLoading) {
-      final Agent<_DecoderState> agent = await _getAgent();
       await _loadMessage(agent, _count);
-      String? decodedMessage = await agent.query((state) => state.lastDecodedMessage);
+      String? decodedMessage =
+          await agent.query((state) => state.lastDecodedMessage);
       if (decodedMessage != null) {
         _decodedMessages.add(decodedMessage);
         setState(() {
@@ -198,7 +218,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    startCounting();
+    startLoadingMessages();
     super.initState();
   }
 
